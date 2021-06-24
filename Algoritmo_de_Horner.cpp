97fe973e0f27bf88fbdf0fabcb1fe23ae4d1ae21
SebastianBaeza <74209913+SebastianBaeza@users.noreply.github.com>
2021-06-23 23:23:58 -0400

    Add files via upload

--- Algoritmo_de_Horner.cpp
+++ Algoritmo_de_Horner.cpp
@@ -2,12 +2,12 @@
 #include "TDA_Lista_Enlazada.cpp"
 
 int Horner(tLista polinomio, int evaluar){
-    tLista temp = polinomio;
+    tLista temp = polinomio; //se copia el valor del polinomio
     polinomio.moveToEnd();
-    int b_n = polinomio.erase().coeficiente;
+    int b_n = polinomio.erase().coeficiente; //se obtiene y borra el valor del coeficiente de la expresion
     while (polinomio.length() != 0){
-        b_n = polinomio.erase().coeficiente + (b_n * evaluar);
+        b_n = polinomio.erase().coeficiente + (b_n * evaluar); //se suman los coeficientes anteriores con la multiplicacion correspondiente
     };
-    polinomio = temp;
+    polinomio = temp; //se restaura el valor original de polinomio 
     return b_n;
 };
--- Problema1.cpp
+++ Problema1.cpp
@@ -8,35 +8,35 @@ using namespace std;
 
 int main(){
     fstream polinomios; 
-    polinomios.open("entradaPolinomio.txt", ios::in);
+    polinomios.open("entradaPolinomio.txt", ios::in);   //se abre el archivo
     if(!polinomios.is_open()){
         cerr << "Error al abrir el archivo\n";
         exit(1); 
     };
     int n_polinomios;
-    polinomios.read((char*)&n_polinomios, sizeof(int));
-    tLista* lista_Polinomios = new tLista[n_polinomios];
+    polinomios.read((char*)&n_polinomios, sizeof(int)); //se lee el numero de polinomios a establecer
+    tLista* lista_Polinomios = new tLista[n_polinomios];    //arreglo de listas que contendran polinomios    
 
     for (int i = 0 ; i <= n_polinomios ; i++){
         int n_monomios;
-        polinomios.read((char*)&n_monomios, sizeof(int));
+        polinomios.read((char*)&n_monomios, sizeof(int)); //se lee el numero de monomios existentes en el polinomio actual
         for (int j = 0 ; j <= n_monomios ; j++){
             int exp;
             int coef;
             monomio nuevo;
-            polinomios.read((char*)&exp, sizeof(int));
-            polinomios.read((char*)&coef, sizeof(int));
+            polinomios.read((char*)&exp, sizeof(int)); //se agrega el exponente del "x"
+            polinomios.read((char*)&coef, sizeof(int)); // se agrega el coeficiente
             nuevo.coeficiente = coef;
             nuevo.exponente = exp;
             lista_Polinomios[i].moveToStart();
-            while (lista_Polinomios[i].currPos() != exp){
+            while (lista_Polinomios[i].currPos() != exp){ //se revisa si el exponente concuerda con la posicion en la lista
                 if (lista_Polinomios[i].currPos() == (lista_Polinomios[i].length()) - 1){
                     monomio vacio;
-                    lista_Polinomios[i].insert(vacio);
+                    lista_Polinomios[i].insert(vacio); //si no lo hace, se llena con un "espacio vacio"
                 };
                 lista_Polinomios[i].next();
             };
-            lista_Polinomios[i].insert(nuevo);
+            lista_Polinomios[i].insert(nuevo); //se inserta el monomio en la posicion correspondiente
         };
     };
 
